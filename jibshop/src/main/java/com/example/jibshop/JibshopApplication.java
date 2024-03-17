bf904bff1edc55216a94d453e34811509ed428a2
weyxou <zibekmakusova@gmail.com>
2024-03-17 15:01:28 +0600

    first commit

---
+++ jibshop/src/main/java/com/example/jibshop/JibshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.jibshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibshopApplication.class, args);
+	}
+
+}
